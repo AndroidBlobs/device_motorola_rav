431e5e580853fe1eb39815a873ffc4cfe41e336b
ShivamKumarJha <jha.shivam3@gmail.com>
2020-03-14 18:28:05 +0000

    rav_retail-user 10 QPJ30.53-16 23451 release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ lineage_rav.mk
@@ -0,0 +1,23 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from rav device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := motorola
+PRODUCT_DEVICE := rav
+PRODUCT_MANUFACTURER := motorola
+PRODUCT_NAME := lineage_rav
+PRODUCT_MODEL := moto g(8)
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+TARGET_VENDOR := motorola
+TARGET_VENDOR_PRODUCT_NAME := rav
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="rav_retail-user 10 QPJ30.53-16 23451 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := motorola/rav_retail/rav:10/QPJ30.53-16/23451:user/release-keys
